741c857457d74800548834d01f92e841aec80416
Joe Innes <joe@joeinn.es>
2021-11-30 00:07:01 +0100

    Update getToken.ts

--- src/lib/auth/getToken.ts
+++ src/lib/auth/getToken.ts
@@ -1,5 +1,9 @@
 export const getToken = async (): Promise<string> => {
-  const res = await fetch(`${import.meta.env.VITE_HOST}/api/auth/token`);
-  const json = await res.json();
-  return json.spotify_access_token;
+  try {
+    const res = await fetch(`${import.meta.env.VITE_HOST}/api/auth/token`);
+    const json = await res.json();
+    return json.spotify_access_token;
+  } catch (e) {
+    return;
+  }
 }
